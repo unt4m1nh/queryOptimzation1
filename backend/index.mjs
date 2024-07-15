398b1fa0ede5f291c5aeac4c68bee18cb8aa50ba
Gia Minh Hoang <giaminh5567@gmail.com>
2024-07-15 22:03:26 +0700

    feat: complete api for get product by name

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,48 +1,67 @@
 // Load environment variables
-import "./loadEnvironment.mjs";
-import express from "express"
-import mongoose from "mongoose";
+import './loadEnvironment.mjs';
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3001;
 
-mongoose.connect('mongodb://localhost:27017/Ecommerce');
+mongoose.connect('mongodb://localhost:27017/Amazon');
 
 const productSchema = mongoose.Schema({
-    ID: Number,
-    Type: String,
-    SKU: String,
-    Name: String,
-    Published: Number,
-    'Is featured ?': Number,
-    'Visibility in catalog': String,
-    'Short description': String,
-    'In stock ?': Number,
-    'Stock': Number,
-    'Backorders allowed ?': Number,
-    'Sold individually ?': Number,
-    'Allow customer reviews?': Number,
-    'Regular price': Number,
-    Categories: String,
-    Images: String,
-    'Attribute 1 name': String,
-    'Attribute 1 value(s)': String,
-    'Attribute 2 name': String,
-    'Attribute 2 value(s)': String
-})
+  _id: String,
+  name: String,
+  main_category: String,
+  sub_category: String,
+  image: String,
+  link: String,
+  ratings: Number,
+  no_of_ratings: Number,
+  discount_price: String,
+  actual_price: String,
+});
 
-const productModel = mongoose.model("Product", productSchema);
+const productModel = mongoose.model('shoes_2', productSchema);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!');
+});
 
-  app.get('/getProducts', async (req, res) => {
-    console.log('Request get products has been made');
-    let result = await productModel.find({});
-    res.send(result).status(200);
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get('/getProducts', async (req, res) => {
+  console.log('Request get products has been made');
+  let result = await productModel.find({}).limit(20);
+  res.send(result).status(200);
+});
+
+/**
+ * Get products by text search
+ */
+app.get('/getProductsByName/:name', async (req, res) => {
+  const name = req.params.name.split(':').at(1);
+  let result = await productModel
+    .find({
+      // name: { $regex: name },
+      $text: { $search: name },
+    })
+    .limit(20);
+  res.send(result).status(200);
+});
+
+/**
+ * Get query execution stats
+ */
+app.get('/getProductsByName/executionStats/:name', async (req, res) => {
+  const name = req.params.name.split(':').at(1);
+  let result = await productModel
+    .find({
+      // name: { $regex: name },
+      $text: { $search: name },
+    })
+    .limit(20)
+    .explain('executionStats');
+  res.send(result).status(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

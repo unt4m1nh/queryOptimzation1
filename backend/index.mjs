f84574d77b5972191dcb80beca73f2259a433224
Gia Minh Hoang <giaminh5567@gmail.com>
2024-07-16 10:47:23 +0700

    feat: add pagination query

--- backend/index.mjs
+++ backend/index.mjs
@@ -21,29 +21,36 @@ const productSchema = mongoose.Schema({
   actual_price: String,
 });
 
-const productModel = mongoose.model('shoes_2', productSchema);
+const productModel = mongoose.model('shoes', productSchema);
+const productsPerPage = 20;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/getProducts', async (req, res) => {
+app.get('/getProducts/page?', async (req, res) => {
   console.log('Request get products has been made');
-  let result = await productModel.find({}).limit(20);
+  const pageNum = req.query.page;
+  let result = await productModel
+    .find({})
+    .skip((pageNum - 1) * productsPerPage)
+    .limit(productsPerPage);
   res.send(result).status(200);
 });
 
 /**
  * Get products by text search
  */
-app.get('/getProductsByName/:name', async (req, res) => {
+app.get('/getProductsByName/:name/page?', async (req, res) => {
   const name = req.params.name.split(':').at(1);
+  const pageNum = req.query.page;
   let result = await productModel
     .find({
       // name: { $regex: name },
       $text: { $search: name },
     })
-    .limit(20);
+    .skip((pageNum - 1) * productsPerPage)
+    .limit(productsPerPage);
   res.send(result).status(200);
 });
 
@@ -52,12 +59,15 @@ app.get('/getProductsByName/:name', async (req, res) => {
  */
 app.get('/getProductsByName/executionStats/:name', async (req, res) => {
   const name = req.params.name.split(':').at(1);
+  const pageNum = req.query.page;
   let result = await productModel
     .find({
       // name: { $regex: name },
       $text: { $search: name },
     })
     .limit(20)
+    .skip((pageNum - 1) * productsPerPage)
+    .limit(productsPerPage)
     .explain('executionStats');
   res.send(result).status(200);
 });
